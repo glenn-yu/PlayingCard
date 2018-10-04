1c1dde152de04753687d0d23d96544688adf2ba5
Yong <gwangy.yu@gmail.com>
2018-10-04 15:18:30 +0900

    Lecture 6 Multitouch

--- PlayingCard.swift
+++ PlayingCard.swift
@@ -17,18 +17,18 @@ struct PlayingCard: CustomStringConvertible
     var rank: Rank
     
     enum Suit: String, CustomStringConvertible {
-        var description: String
         
-        case spades = "♠︎"
-        case hearts = "❤️"
-        case clubs = "♧"
-        case diamonds = "🔷"
+        case spades = "♠️"
+        case hearts = "♥️"
+        case clubs = "♣️"
+        case diamonds = "♦️"
         
         static var all = [Suit.spades,.hearts,.diamonds,.clubs]
+        
+        var description: String { return rawValue }
     }
     
     enum Rank: CustomStringConvertible {
-        var description: String
         
         case ace
         case face(String)
@@ -54,6 +54,14 @@ struct PlayingCard: CustomStringConvertible
             allRanks += [Rank.face("J"),.face("Q"),.face("K")]
             
             return allRanks
+        }
+        
+        var description: String {
+            switch self {
+            case .ace: return "A"
+            case .numeric(let pips): return String(pips)
+            case .face(let kind): return kind
+            }
         }
     }
 }
